a86b078cd676460ff8cc6aeaf792638867d4f963
ahmfarisi <ahmfarisi@gmail.com>
2022-09-21 14:22:16 +0700

    update MainActivity

--- app/src/main/java/com/if3b/mahasiswabaru/MainActivity.java
+++ app/src/main/java/com/if3b/mahasiswabaru/MainActivity.java
@@ -3,12 +3,22 @@ package com.if3b.mahasiswabaru;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNomorPendaftaran;
+    EditText etNama;
+    Button btnDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNomorPendaftaran = findViewById(R.id.et_nomor_pendaftaran);
+        etNama = findViewById(R.id.et_nama);
+        btnDaftar = findViewById(R.id.btn_daftar);
+        
     }
 }
